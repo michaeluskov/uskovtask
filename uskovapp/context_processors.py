2e4462247d100a949f37ca7ebe52849d411990c3
Michael Uskov <uskovm@gmail.com>
2014-11-27 21:10:58 +0500

    Moved context processor's methods to visits.py

--- uskovapp/context_processors.py
+++ uskovapp/context_processors.py
@@ -1,25 +1,10 @@
 from models import Visits
-from django.db.models import Q
-from datetime import timedelta
-
-
-def getSessionsQuerySet():
-    sessions = []
-    for ip in Visits.objects.values('ip').order_by().distinct():
-        ip = ip['ip']
-        allvisits = list(Visits.objects.filter(ip__exact=ip).values('datetime'))
-        sessions.append({'ip': ip, 'datetime': allvisits[0]})
-        allvisits = [i['datetime'] for i in allvisits]
-        for second in xrange(1, len(allvisits)):
-            first = second-1
-            if (allvisits[second] - allvisits[first] >= timedelta(minutes=5)):
-                sessions.append({'ip': ip, 'datetime': allvisits[second]})
-    return sessions
+from visits import getSessionsQuerySet, getAllIPsCount, getAllHitsCount
 
 
 def addstats(request):
-    allvisits = Visits.objects.count()
-    ips = Visits.objects.values('ip').order_by().distinct().count()
+    allvisits = getAllHitsCount()
+    ips = getAllIPsCount()
     sessionsQuerySet = getSessionsQuerySet()
     allSessions = len(sessionsQuerySet)
       
--- uskovapp/visits.py
+++ uskovapp/visits.py
@@ -1,10 +1,29 @@
 from models import Visits
-import datetime
+from datetime import datetime, timedelta
 
 
 def addNewVisit(request):
     visit = Visits()
     visit.ip = request.META['REMOTE_ADDR']
     visit.url = request.path
-    visit.datetime = datetime.datetime.now()
+    visit.datetime = datetime.now()
     visit.save()    
+    
+def getSessionsQuerySet():
+    sessions = []
+    for ip in Visits.objects.values('ip').order_by().distinct():
+        ip = ip['ip']
+        allvisits = list(Visits.objects.filter(ip__exact=ip).values('datetime'))
+        sessions.append({'ip': ip, 'datetime': allvisits[0]})
+        allvisits = [i['datetime'] for i in allvisits]
+        for second in xrange(1, len(allvisits)):
+            first = second-1
+            if (allvisits[second] - allvisits[first] >= timedelta(minutes=5)):
+                sessions.append({'ip': ip, 'datetime': allvisits[second]})
+    return sessions
+
+def getAllIPsCount():
+    return Visits.objects.values('ip').order_by().distinct().count()
+
+def getAllHitsCount():
+    return Visits.objects.count()
