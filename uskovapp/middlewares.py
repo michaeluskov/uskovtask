11d6b6621d361a416f727a35c892509f5f4d8df3
Michael Uskov <uskovm@gmail.com>
2014-12-03 00:22:36 +0500

    Added ErrorMiddleware for handling exceptions

--- uskovapp/middlewares.py
+++ uskovapp/middlewares.py
@@ -1,6 +1,8 @@
 from models import Visits
 from visits import addNewVisit
 import datetime 
+import traceback
+import sys
 
 
 class VisitsMiddleware:
@@ -8,3 +10,11 @@ class VisitsMiddleware:
     def process_request(self, request):
         addNewVisit(request)
         return None
+    
+    
+class ErrorMiddleware:
+    
+    def process_exception(self, request, exception):
+        sys.stderr.write(traceback.format_exc())
+        sys.stderr.flush()
+        
--- uskovtask/settings.py
+++ uskovtask/settings.py
@@ -41,6 +41,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'uskovapp.middlewares.ErrorMiddleware',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
