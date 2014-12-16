bdfbdce70ac1d4452c3189dd8edb5a8523b830af
Michael Uskov <uskovm@gmail.com>
2014-12-16 20:58:39 +0500

    Changed size of pic

--- uskovapp/poll_picture.py
+++ uskovapp/poll_picture.py
@@ -12,7 +12,7 @@ def createPollPic(poll_id):
     poll_text = Polls.objects.filter(pk=poll_id).values('text')[:1][0]['text']
     variants = PollVariants.objects.filter(poll_id=poll_id).annotate(votes_num=Count('votes'))
     max_votes_num = max([i.votes_num for i in variants]) or 1   
-    image = PIL.Image.new('RGB',(500, 15+len(variants)*50), '#ffffff')
+    image = PIL.Image.new('RGB',(440, 30+len(variants)*16), '#ffffff')
     font = PIL.ImageFont.truetype('uskovapp/Helvetica_Light-Normal.ttf', size=18, encoding='utf-8')
     draw = PIL.ImageDraw.Draw(image)
     draw.text((15,3), poll_text, fill='#000000', font=font)
